a93a113cdfbf8d52e1512cdfa7e77a1da8325d63
Salvatore <salvatorebono2001@gmail.com>
2023-10-16 18:49:44 +0200

    add props

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,14 +1,40 @@
 <script>
 export default {
+  props: {
+    project: { type: Object },
+  },
   components: {},
   data() {
     return {};
   },
-  methods: {},
+  methods: {
+    //getImg(project) costruisce l'URL completo per l'immagine utilizzando l'indirizzo base "http://127.0.0.1:8000/storage/" e il nome del file immagine del progetto.
+    getImg(project) {
+      return `http://127.0.0.1:8000/storage/${project.thumbnail}`;
+    },
+  },
   mounted() {},
 };
 </script>
 
-<template>CIAO SONO LA CARD</template>
+<template>
+  <img :src="getImg(project)" class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">{{ project.title }}</h5>
+    <p class="card-text">
+      {{ project.description }}
+    </p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">{{ project.date }}</li>
+    <li class="list-group-item">{{ project.type.type }}</li>
+    <li v-for="technology in project.technologies" class="list-group-item">
+      {{ technology.name }}
+    </li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">{{ project.link }}</a>
+  </div>
+</template>
 
 <style lang="scss" scoped></style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -17,10 +17,6 @@ export default {
         this.projects = response.data.data;
       });
     },
-    //getImg(project) costruisce l'URL completo per l'immagine utilizzando l'indirizzo base "http://127.0.0.1:8000/storage/" e il nome del file immagine del progetto.
-    getImg(project) {
-      return `http://127.0.0.1:8000/storage/${project.thumbnail}`;
-    },
   },
   mounted() {
     this.getApi();
@@ -32,26 +28,7 @@ export default {
   <main>
     <div class="container pt-5 d-flex gap-4">
       <div v-for="project in projects" class="card" style="width: 18rem">
-        <img :src="getImg(project)" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">{{ project.title }}</h5>
-          <p class="card-text">
-            {{ project.description }}
-          </p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">{{ project.date }}</li>
-          <li class="list-group-item">{{ project.type.type }}</li>
-          <li
-            v-for="technology in project.technologies"
-            class="list-group-item"
-          >
-            {{ technology.name }}
-          </li>
-        </ul>
-        <div class="card-body">
-          <a href="#" class="card-link">{{ project.link }}</a>
-        </div>
+        <ProjectCard :project="project"></ProjectCard>
       </div>
     </div>
   </main>
