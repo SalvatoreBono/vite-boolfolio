1de16e31b470577676bcca75b2684f52860ba566
Salvatore <salvatorebono2001@gmail.com>
2023-10-16 19:54:38 +0200

    fix pagination

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -9,13 +9,19 @@ export default {
   data() {
     return {
       projects: [],
+      pagination: {},
     };
   },
   methods: {
-    getApi() {
-      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
-        this.projects = response.data.data;
-      });
+    getApi(url) {
+      axios
+        //se l'argomento "url" non è definito, verrà utilizzata l'URL predefinito ovvero "http://127.0.0.1:8000/api/projects"
+        .get(url ?? "http://127.0.0.1:8000/api/projects")
+        .then((response) => {
+          const results = response.data;
+          this.projects = results.data;
+          this.pagination = results;
+        });
     },
   },
   mounted() {
@@ -26,9 +32,24 @@ export default {
 
 <template>
   <main>
-    <div class="container pt-5 d-flex gap-4">
-      <div v-for="project in projects" class="card" style="width: 18rem">
-        <ProjectCard :project="project"></ProjectCard>
+    <div class="container pt-5">
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div v-for="project in projects" class="col">
+          <div class="card" style="width: 18rem">
+            <ProjectCard :project="project"></ProjectCard>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="text-center pt-5">
+          <!-- @click="getApi(link.url) =  Quando viene cliccato, richiama il metodo getApi con l'URL specificato nell'elemento link.url -->
+          <a
+            v-for="link in pagination.links"
+            @click="getApi(link.url)"
+            class="btn btn-primary me-1 ms-1"
+            v-html="link.label"
+          ></a>
+        </div>
       </div>
     </div>
   </main>
