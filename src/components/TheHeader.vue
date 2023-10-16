a5abe5f36146de7926dc98b100b9b8d94974a0bb
Salvatore <salvatorebono2001@gmail.com>
2023-10-16 22:51:55 +0200

    add style

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -9,6 +9,83 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-navabar">
+      <div class="container-fluid">
+        <div>
+          <a class="navbar-brand" href="#">
+            <img
+              src="../assets/image.png"
+              alt="Bootstrap"
+              width="40"
+              height="40"
+            />
+          </a>
+        </div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <div class="pt-1 pb-1">
+            <i class="fa-solid fa-bars fa-lg" style="color: #efeff1"></i>
+          </div>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarNavDropdown"
+        >
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Projects</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../scss/partials/variables" as *;
+header {
+  font-weight: 600;
+}
+.bg-navabar {
+  background-color: $color-primary;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.9), 0 0px 2px rgba(0, 0, 0, 0.9);
+  a {
+    color: $color-secondary;
+  }
+  a:hover {
+    color: $color-tertiary;
+  }
+}
+
+.nav-link:focus {
+  color: $color-secondary;
+}
+.navbar-nav .nav-link.show {
+  color: $color-tertiary;
+}
+.navbar-toggler {
+  border: 1px solid white;
+}
+.navbar-toggler:focus {
+  box-shadow: 0px 0px 15px 2px rgba(191, 148, 255);
+}
+</style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -31,7 +31,66 @@ export default {
 </script>
 
 <template>
-  <main>
+  <main
+    class="overflow-auto vh-100 bg-home-page text-center text-lg-start align-items-center justify-content-center"
+  >
+    <div class="container d-flex align-items-center vh-100">
+      <div
+        class="row flex-column-reverse flex-lg-row justify-content-center align-items-center"
+      >
+        <div class="col-12 col-md-12 col-lg-6">
+          <h3 class="display-5 fw-bold">Hi, It's Me</h3>
+          <h1 class="display-1 fw-bold">
+            I'm <span style="color: #bf94ff">Salvatore</span>
+          </h1>
+          <p class="col-lg-8 fs-4">
+            I'm a Junior Full Stack Web Developer, I'm excited to share with you
+            the projects and skills I've been honing on my journey in the world
+            of web development. My goal is continue learning and growing in this
+            dynamic field.
+          </p>
+
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            <a
+              class="nav-link"
+              target="_blank"
+              href="https://www.linkedin.com/in/salvatore-bono-692824255/"
+            >
+              <div
+                class="link-profile d-flex align-items-center justify-content-center"
+              >
+                <i class="fa-brands fa-linkedin-in fa-xl"></i>
+              </div>
+            </a>
+            <a
+              class="nav-link pe-3 ps-3"
+              target="_blank"
+              href="https://www.instagram.com/salvatore__bono/"
+            >
+              <div
+                class="link-profile d-flex align-items-center justify-content-center"
+              >
+                <i class="fa-brands fa-instagram fa-xl"></i>
+              </div>
+            </a>
+            <a
+              class="nav-link"
+              target="_blank"
+              href="https://github.com/SalvatoreBono"
+            >
+              <div
+                class="link-profile d-flex align-items-center justify-content-center"
+              >
+                <i class="fa-brands fa-github fa-xl"></i>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-12 col-md-12 col-lg-6 pb-3 pb-lg-0">
+          <img class="media-width" src="../assets/image.png" alt="" />
+        </div>
+      </div>
+    </div>
     <div class="container pt-5">
       <div class="row row-cols-1 row-cols-md-3 g-4">
         <div v-for="project in projects" class="col">
@@ -55,4 +114,31 @@ export default {
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../scss/partials/variables" as *;
+
+.bg-home-page {
+  background-color: #0e0e10;
+  color: $color-secondary;
+}
+.link-profile {
+  width: 45px;
+  height: 45px;
+  border: 1px solid $color-secondary;
+  border-radius: 50%;
+  transition: 0.5s;
+}
+.link-profile:hover {
+  background-color: $color-tertiary;
+  box-shadow: 0px 0px 15px 2px rgba(191, 148, 255);
+}
+.media-width {
+  width: 75%;
+}
+
+@media screen and (max-width: 992px) {
+  .media-width {
+    width: 43%;
+  }
+}
+</style>
